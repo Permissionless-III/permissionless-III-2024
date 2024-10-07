b730bb9282d7ea48e8169a217188c5ea2cd626f5
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2024-10-07 15:39:10 -0600

    Added update to access token.

--- web/src/app/actions/kyc.ts
+++ web/src/app/actions/kyc.ts
@@ -1,5 +1,5 @@
-const crypto = require('crypto');
 "use server";
+const crypto = require('crypto');
 
 export async function generateAccessToken(): Promise<string> {
   const SUMSUB_TOKEN = process.env.SUMSUB_TOKEN;
@@ -14,18 +14,22 @@ export async function generateAccessToken(): Promise<string> {
   }
 
   const timestamp = Math.floor(Date.now() / 1000);
-  const signature = crypto.createHmac('sha256', SUMSUB_SECRET_KEY).update(timestamp + 'POST' + 'https://api.sumsub.com').digest('hex');
+  const signature = crypto.createHmac('sha256', SUMSUB_SECRET_KEY)
+    .update(timestamp + 'POST' + 'https://api.sumsub.com')
+    .digest('hex');
 
+  console.log();
   console.log(SUMSUB_TOKEN);
+  console.log(SUMSUB_SECRET_KEY);
   console.log(signature);
+  console.log();
 
   const response = await fetch("https://api.sumsub.com/resources/accessTokens", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
       "X-App-Token": SUMSUB_TOKEN,
       "X-App-Access-Sig": signature,
-      "X-App-Access-Ts": timestamp,
+      "X-App-Access-Ts": timestamp.toString(),
     },
   });
 
