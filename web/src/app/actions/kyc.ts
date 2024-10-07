047ebed1b8b08408ddcb2ed88666efca7d9e8848
Kristie Giles <klgslc@gmail.com>
2024-10-07 14:01:42 -0600

    update

--- web/src/app/actions/kyc.ts
+++ web/src/app/actions/kyc.ts
@@ -1,6 +1,12 @@
 "use server";
 
 export async function generateAccessToken(): Promise<string> {
+  const SUMSUB_SECRET_KEY = process.env.SUMSUB_SECRET_KEY;
+
+  if (!SUMSUB_SECRET_KEY) {
+    throw new Error("SUMSUB_SECRET_KEY is not set");
+  }
+
   // TODO: Implement your actual token generation logic here
   return "1232133";
 }
