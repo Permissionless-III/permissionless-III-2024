a2ec037c8e34437d6cecbc1edc50b5d896b96767
Kristie Giles <klgslc@gmail.com>
2024-10-08 15:50:27 -0600

    updates

--- web/src/components/elections/Elections.tsx
+++ web/src/components/elections/Elections.tsx
@@ -1,37 +1,32 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { useContractRead } from "wagmi";
+import { useReadContract } from "wagmi";
 import { Election } from "../types/types";
 import { electionsData } from "@/app/data/elections";
 import Link from "next/link";
+import { ELECTION_FACTORY_CONTRACT_CONFIG } from "@/constants/config";
 
 export default function Elections() {
-  const [elections, setElections] = useState<Election[]>([]);
+  // const [elections, setElections] = useState<Election[]>([]);
 
-  // const { data, isError, isLoading } = useContractRead({
-  //   address: contractAddress,
-  //   abi: contractABI,
-  //   functionName: "getVoteResults",
-  // });
+  const { data: elections, error } = useReadContract({
+    ...ELECTION_FACTORY_CONTRACT_CONFIG,
+    functionName: "elections",
+  });
 
-  useEffect(() => {
-    if (electionsData) {
-      // Assuming the contract returns an array of objects with option and votes
-      setElections(electionsData);
-    }
-  }, [elections]);
-
+  console.log("elections", elections);
+  console.log("error", error);
   // if (isLoading) return <div>Loading results...</div>;
   // if (isError) return <div>Error loading results</div>;
 
   return (
     <div>
-      {elections.length === 0 ? (
+      {!elections || elections?.length === 0 ? (
         <p>No elections have been created yet.</p>
       ) : (
         <div className="flex flex-col gap-3 h-full">
-          {elections.map(election => (
+          {elections.map((election: any) => (
             <Link
               className="bg-white shadow-md p-4 rounded-xl w-full block"
               key={election.id}
