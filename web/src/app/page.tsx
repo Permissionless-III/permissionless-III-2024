cf426b75c659c83c7f8692d1acca206671290915
Kristie Giles <klgslc@gmail.com>
2024-10-07 20:48:53 -0600

    fix import issue

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+import dynamic from "next/dynamic";
 import Head from "next/head";
 import "@/lib/env";
 /**
@@ -9,9 +11,11 @@ import "@/lib/env";
  * You can override the next-env if the type is important to you
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
-import Kyc from "@/components/Kyc";
 import Vote from "@/components/vote/Vote";
-import { useState } from "react";
+
+const Kyc = dynamic(() => import("@/components/Kyc"), {
+  ssr: false,
+});
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
