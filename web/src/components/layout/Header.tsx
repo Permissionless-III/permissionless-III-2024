02a679292d3eb1a6908e5b3e0f5eaff3712ca714
Kristie Giles <klgslc@gmail.com>
2024-10-07 21:02:10 -0600

    make sumsub wider

--- web/src/components/layout/Header.tsx
+++ web/src/components/layout/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import { ConnectKitButton } from "connectkit";
+import { useAccount } from "wagmi";
 
 export default function Header() {
+  const { address } = useAccount();
   return (
     <div className="flex justify-end items-center p-4 bg-white sticky top-0 z-50">
-      <ConnectKitButton />
+      {address && <ConnectKitButton />}
     </div>
   );
 }
