06d2278b09d8783e6f103c704000c1a3f288c1e4
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2024-10-07 18:00:04 -0600

    Fixed access token.

--- web/src/__tests__/utils/api.test.tsx
+++ web/src/__tests__/utils/api.test.tsx
@@ -2,7 +2,7 @@ import { generateAccessToken } from '@/app/actions/kyc';
 
 describe('Test Generating Access Token', () => {
   it('should generate access token', async () => {
-    const response = await generateAccessToken();
+    const response = await generateAccessToken('basic-kyc-level', 'jon-jones');
     console.log(response);
     expect(response.code).toBe(200);
   });
