b8838c997663478db45fcf5619d75649bde1b682
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2024-10-08 12:33:36 -0600

    Added create and edit election pages.

---
+++ web/src/app/create/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import Create from "@/components/create/Create";
+
+export default function CreatePage() {
+  return <Create />;
+}
---
+++ web/src/components/create/Create.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { Election } from "@/components/types/types";
+
+export default function Create() {
+  // const [elections, setElections] = useState<Election[]>([]);
+
+  return (
+    <div>
+      <h2>Create Election</h2>
+    </div >
+  );
+}
