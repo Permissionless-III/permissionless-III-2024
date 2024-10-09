02c85bb9309208aa104deb9fde2f534c752edcea
Kristie Giles <klgslc@gmail.com>
2024-10-08 21:04:01 -0600

    cleanup

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,14 +2,10 @@ import { Metadata } from "next";
 import * as React from "react";
 
 import "@/styles/globals.css";
-// !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
-import "@/styles/colors.css";
 
 import { siteConfig } from "@/constants/config";
 import Layout from "@/components/layout/Layout";
 
-// !STARTERCONF Change these default meta
-// !STARTERCONF Look at @/constant/config to change them
 export const metadata: Metadata = {
   metadataBase: new URL(siteConfig.url),
   title: {
@@ -40,14 +36,7 @@ export const metadata: Metadata = {
     title: siteConfig.title,
     description: siteConfig.description,
     images: [`${siteConfig.url}/images/og.jpg`],
-    // creator: '@th_clarence',
   },
-  // authors: [
-  //   {
-  //     name: 'Theodorus Clarence',
-  //     url: 'https://theodorusclarence.com',
-  //   },
-  // ],
 };
 
 export default function RootLayout({
