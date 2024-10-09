52eba97f2e3eda8bae155827a2eae0a4af287387
Kristie Giles <klgslc@gmail.com>
2024-10-08 18:33:13 -0600

    rootstock

--- web/src/components/layout/Web3Provider.tsx
+++ web/src/components/layout/Web3Provider.tsx
@@ -1,28 +1,19 @@
 import { WagmiProvider, createConfig, http } from "wagmi";
-import { mainnet, sepolia } from "wagmi/chains";
+import { mainnet, rootstock, rootstockTestnet } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 
 const config = createConfig(
   getDefaultConfig({
-    // Your dApps chains
-    chains: [sepolia],
+    chains: [rootstock, rootstockTestnet],
     transports: {
-      // RPC URL for each chain
-      [sepolia.id]: http(
-        `https://sepolia.infura.io/v3/e302133655264f39baaf743ce6554333`
-      ),
+      [rootstock.id]: http(`https://public-node.rsk.co/`),
+      [rootstockTestnet.id]: http(`https://public-node.testnet.rsk.co`),
     },
-
-    // Required API Keys
     walletConnectProjectId: process.env
       .NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
 
-    // Required App Info
-    appName: "Permissionless Voting",
-
-    // Optional App Info
-    appDescription: "Your App Description",
+    appName: "BitVote",
     ssr: true,
     // appUrl: "https://family.co", // your app's url
     // appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
--- web/src/components/vote/VoteSuccess.tsx
+++ web/src/components/vote/VoteSuccess.tsx
@@ -13,7 +13,7 @@ export default function VoteSuccess() {
         <div className="mb-2">Your vote has been cast!</div>
         <Link
           className="font-medium text-primary-500 inline-flex items-center"
-          href={`https://sepolia.etherscan.io/tx/`}
+          href={`https://rootstock-testnet.blockscout.com/tx`}
         >
           View on Block Explorer
           <ArrowRightIcon className="w-5 h-5" />
